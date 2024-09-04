23f494a54e093ed3eff477c4bef676eff44ce51e
raut-sanket <124782031+raut-sanket@users.noreply.github.com>
2024-09-04 16:42:08 +0530

    Create medusa-config.js

---
+++ medusa-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  projectConfig: {
+    // ...
+    database_extra: process.env.NODE_ENV !== "development" ?
+      {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      } : {},
+  },
+}
